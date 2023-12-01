a2dc53a35720843f30f457e6d15364c696c1743f
bphphin <buingocphinn@gmail.com>
2023-12-01 14:52:35 +0700

    feat: laravel breeze package

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
-use App\Http\Controllers\AirlineController;
-use App\Http\Controllers\BrandController;
-use App\Http\Controllers\CarController;
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\FlightController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,14 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.app');
+    return view('welcome');
 });
 
-Route::resource('post', PostController::class);
-Route::resource('student', StudentController::class);
-Route::resource('product', ProductController::class);
-Route::resource('client', ClientController::class);
-Route::resource('brand', BrandController::class);
-Route::resource('car', CarController::class);
-Route::resource('airline', AirlineController::class);
-Route::resource('flight', FlightController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
